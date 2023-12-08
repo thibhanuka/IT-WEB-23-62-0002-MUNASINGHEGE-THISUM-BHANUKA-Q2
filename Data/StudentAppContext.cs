65c87909b608ad4e19ba92551f1ee3308393dcb7
thibhanuka <129237747+thibhanuka@users.noreply.github.com>
2023-12-09 01:18:21 +0530

    Add project files.

---
+++ Data/StudentAppContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Model;
+
+namespace StudentApp.Data
+{
+    public class StudentAppContext : DbContext
+    {
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>()
+                .HasOne(s => s.Course)
+                .WithMany(c => c.Students)
+                .HasForeignKey(s => s.CourseId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+        
+
+        public StudentAppContext (DbContextOptions<StudentAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentApp.Model.Student> Student { get; set; } = default!;
+
+        public DbSet<StudentApp.Model.Course> Course { get; set; } = default!;
+    }
+}
---
+++ Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Data;
+using StudentApp.Model;
+
+namespace StudentApp.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly StudentApp.Data.StudentAppContext _context;
+
+        public IndexModel(StudentApp.Data.StudentAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Course != null)
+            {
+                Course = await _context.Course.ToListAsync();
+            }
+        }
+    }
+}
